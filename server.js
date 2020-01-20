a6fa20e4b8ec54e0fdd038e1ff1e3fcf16ed5699
aamku <Aamku@lifestyle1>
2020-01-20 15:56:09 +0530

    some changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(require('./routes/addUser.js'));
 app.use(require('./routes/allUsers.js'));
 app.use(require('./routes/sendOtp.js'));
+app.use(require('./routes/checkOtp.js'));
 
 app.listen(port,() => {
 
