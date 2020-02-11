0cf9092710c4597ce89e549054f017483e3ab888
aamku <Aamku@lifestyle1>
2020-02-11 16:59:38 +0530

    some changes

--- routes/salesOrderHis.js
+++ routes/salesOrderHis.js
@@ -9,7 +9,7 @@ const dburl = process.env.URL;
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
 
-router.get('/salesOrderHis',(req,res) => {
+router.post('/salesOrderHis',(req,res) => {
 
     MongoClient.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true},(err,client) => {
 
@@ -25,7 +25,7 @@ router.get('/salesOrderHis',(req,res) => {
                     else{
 
                         const coll = client.db('Aamku_connect').collection('Orders');
-                        coll.find({$and:[{salesperson_id:data.id},{$or:[{order_status:data.status},{order_status:data.stats}]}]}).toArray((err,result) => {
+                        coll.find({$or:[{salesperson_id:data.id},{order_status:data.status},{order_status:data.stats}]}).toArray((err,result) => {
 
                              if(err){
                                  console.log("Error",err);
