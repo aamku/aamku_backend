3f8cf89543f4fe74d1dcb3ca41aacedc6a8f422c
aamku <Aamku@lifestyle1>
2020-02-11 16:14:26 +0530

    some changes

--- routes/placeOrder.js
+++ routes/placeOrder.js
@@ -28,7 +28,7 @@ router.post('/placeOrder',(req,res) => {
                mobile:req.body.mobil
            };
 
-           const phone = "91"+data.mobile;
+           const phone = "91"+req.body.mobil;
            const from = 'Nexmo';
            const message = 'Thanks your order has been placed successfully';
       
